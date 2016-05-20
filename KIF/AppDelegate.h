9f871913cdf218140def9dfd5b1c262950f6d1b7
Jessie Serrino <Jserrino@onjack.com>
2016-05-20 16:27:04 -0700

    Initial Commit

---
+++ KIF/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KIF
+//
+//  Created by Jessie Serrino on 5/20/16.
+//  Copyright © 2016 Jack Mobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KIF/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KIF
+//
+//  Created by Jessie Serrino on 5/20/16.
+//  Copyright © 2016 Jack Mobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
